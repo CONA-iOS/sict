189c3c6066b1ccbc85b87985fe18a81397a62744
lgvv <lgw9898@naver.com>
2022-11-22 15:37:36 +0900

    [추가] 프로젝트 기본 환경세팅

---
+++ SICT/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SICT
+//
+//  Created by Hamlit Jason on 2022/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
